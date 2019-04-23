f244eecaac2c82b1b58e5e5de14e9266a5b05619
chad-stogner <35505994+chad-stogner@users.noreply.github.com>
2019-04-23 00:05:51 -0700

    Add files via upload

---
+++ catan_service.js
@@ -0,0 +1,29 @@
+/**
+catan_service.js
+Chad Stogner
+CSC 337, Spring 2019
+Assignment #11
+
+Web Service accessed by catan.js
+
+
+**/
+
+"use strict";
+
+const express = require("express");
+const app = express();
+
+const fs = require("fs");
+
+app.use(express.static('public'));
+
+/**
+
+**/
+app.get('/', function (req, res) {
+	res.header("Access-Control-Allow-Origin", "*");
+	
+})
+
+app.listen(3000);
